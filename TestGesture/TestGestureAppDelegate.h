90855898caece536d1d27cdb71b00a6ab0926a27
bird <holin20@gmail.com>
2012-02-19 17:29:30 +0800

    Initial Commit

---
+++ TestGesture/TestGestureAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TestGestureAppDelegate.h
+//  TestGesture
+//
+//  Created by bird on 12/2/19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestGestureAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestGesture/TestGestureViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestGestureViewController.h
+//  TestGesture
+//
+//  Created by bird on 12/2/19.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestGestureViewController : UIViewController
+
+@end
